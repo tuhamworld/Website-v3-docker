107d1979e16c69d4ee6007d12bff4a05d21a2d14
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2024-01-24 14:55:36 +0100

    feat: add social atoms

---
+++ src/components/atoms/Socials/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareYoutube } from "react-icons/fa6";
+import { SiUdemy } from "react-icons/si";
+
+const Socials = () => {
+  return (
+    <div className="socials">
+      <a
+        href="https://www.linkedin.com/in/tuhamworld"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+
+      <a href="https://github.com/tuhamworld" target="_blank" rel="noreferrer">
+        <FaGithub />
+          </a>
+          
+          <a href="https://udemy.com/user/tunde-sanusi/" target="_blank" rel="noreferrer">
+              <SiUdemy/>
+          </a>
+
+
+
+          <a href="https://youtube.com/@tuhamworld" target="_blank" rel="noreferrer">
+              <FaSquareYoutube/>
+          </a>
+
+          
+
+    </div>
+  );
+};
+
+export default Socials;
