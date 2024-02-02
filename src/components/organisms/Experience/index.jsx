e9e1b7c7784840c3bd1bdf022db7e2d8508ccf2a
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2024-02-02 20:30:04 +0100

    feat: adding experience

---
+++ src/components/organisms/Experience/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "index.scss";
+
+const Experience = () => {
+  return (
+    <>
+      <section id="experience">
+        <h5>Tech Stack</h5>
+        <h2>Skills</h2>
+        <div className="container experience__container">
+          <div className="experience__frontend">
+                      <h3>Front-end Engineer</h3>
+                      <div className="experience__content">
+                          <article className="experience__details">
+                              
+                          </article>
+                      </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
