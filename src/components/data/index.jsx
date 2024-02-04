57bfd2caa1432e54d18b458248ef12b87637b849
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2024-02-04 08:23:46 +0100

    refactor: portfolio data

---
+++ src/components/data/index.jsx
@@ -0,0 +1,134 @@
+import TheInfoWorth from "../../assets/images/pngs/theinfoworth.png";
+import DictionaryWebApp from "../../assets/images/jpgs/dictionary_app.jpg";
+import WeatherApp from "../../assets/images/jpgs/weather_app.jpg";
+import DynacareHealth from "../../assets/images/pngs/dynacare.png";
+import MemeGenerator from "../../assets/images/pngs/meme-generator.png";
+import Hydro from "../../assets/images/jpgs/hydro-tuh.jpg";
+import ACareReview from "../../assets/images/jpgs/acarereview.jpg";
+import Airbnb from "../../assets/images/jpgs/airbnb.jpg";
+import TheGreenFashion from "../../assets/images/jpgs/thegreenfashion.jpg";
+import ElivaitAcademy from "../../assets/images/pngs/elivait_academy.png";
+import MSSNFUTA from "../../assets/images/pngs/mssnfuta.png";
+import BanSchool from "../../assets/images/jpgs/ban-school.jpg";
+
+const data = [
+  {
+    id: 1,
+    title: "De Dictionary",
+    img: DictionaryWebApp,
+    description:
+      "A mini dictionary web app with word meanings, word pronouncitations, audio word pronouciations, and word examples",
+    technologies: "JavaScript | API | HTML | CSS",
+    link: "https://de-dictionary.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "The Info Worth",
+    img: TheInfoWorth,
+    description:
+      "Online Magazine Website on Entreprenership, Net Worth, Business Ideas and MMO ",
+    technologies: "WordPress, CMS | SEO | CSS3",
+    link: "https://theinfoworth.com/",
+  },
+
+  {
+    id: 3,
+    title: "Airbnb Experiences",
+    img: Airbnb,
+    description:
+      "A fictious replica of the popular Airbnb Proptech with unique Experiences ",
+    technologies: "ReactJS | JavaScript | CSS3 | Props",
+    link: "https://de-airbnb-experiences.vercel.app/",
+  },
+
+  {
+    id: 4,
+    title: "A Care Review",
+    img: ACareReview,
+    description:
+      "An educational website which provide reviews of  universities in West Africa",
+    technologies: "JavaScript | SCSS | JS Components | CSS3",
+    link: "https://acarereview.vercel.app/",
+  },
+
+  {
+    id: 5,
+    title: "The Green Fashion Factory",
+    img: TheGreenFashion,
+    description:
+      "A notable source for all eco-friendly and sustainable arts and crafts - art pieces, shoes, bags, and accessories from what is considered “waste”.",
+    technologies: "WordPress, CMS | CSS3 | Pixel-perfect | CSS3 | Elementor ",
+    link: "https://thegreenfashion.org/",
+  },
+
+  {
+    id: 6,
+    title: "Meme Generator",
+    img: MemeGenerator,
+    description: "A comical web app for generating memes to make your day",
+    technologies: "ReactJS | JavaScript | API | Vercel ",
+    link: "https://github.com/tuhamworld/meme-generator",
+  },
+
+  {
+    id: 7,
+    title: "De Weather App",
+    img: WeatherApp,
+    description:
+      "A dynamic web app for showing the current weather of any geographical area",
+    technologies: "JavaScript | API | CSS3 | HTML5 ",
+    link: "https://de-weather.vercel.app/",
+  },
+
+  {
+    id: 8,
+    title: "DynaCare",
+    img: DynacareHealth,
+    description:
+      "An NDIS and TAC Registered Provider, founded by a team of experienced Medical professionals and Personal Care workers to deliver person-centred care.",
+    technologies: "WordPress | Pixel-Perfect | CSS3 | SEO ",
+    link: "https://dynacare.net.au/",
+  },
+
+  {
+    id: 9,
+    title: "Hydro",
+    img: Hydro,
+    description:
+      "Hydro is a fictious responsive project focusing on making beautiful websites for all people",
+    technologies: "JavScript | Pixel-Perfect | CSS3 | HTML5 ",
+    link: "https://hydro-tuh.vercel.app/",
+  },
+
+  {
+    id: 10,
+    title: "Elivait Academy",
+    img: ElivaitAcademy,
+    description:
+      "Elivait Academy is an elearning platform that empower people to learn vocational, technical, digital and soft skills",
+    technologies: "LMS | Payment Integration | API | eCommerce ",
+    link: "https://elivait.com.ng/",
+  },
+
+  {
+    id: 11,
+    title: "Ban School",
+    img: BanSchool,
+    description:
+      "British American Nursing School is an healthcare staffing agency providing opportunity for nurses to work in the United Kingdom, United States and Canada",
+    technologies: "Business | WordPress, CMS | Forms API ",
+    link: "https://ban-school.com/",
+  },
+
+  {
+    id: 12,
+    title: "MSSN FUTA",
+    img: MSSNFUTA,
+    description:
+      "An islamic non-profit organization meant to cater for the welfare of Muslim students on Campus",
+    technologies: "Pixel-Perfect | File-sharing | Contact Automation | WordPress, CMS ",
+    link: "https://mssnfuta.com/",
+  },
+];
+
+export default data;
